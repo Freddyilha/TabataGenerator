dfbcf5655a058ad6d604caa3873c26071147ae65
Freddy <freddyilha@gmail.com>
2021-09-07 12:12:06 -0300

    fix: litte refactoring

--- converter.py
+++ converter.py
@@ -1,13 +1,23 @@
-import os
 from pydub import AudioSegment
 from pydub.playback import play
 
-def main():
+class Converter:
+    def generate(self):
 
-    track = AudioSegment.from_file("Songs/Track-1.mp3", "mp3")
+        ten_seconds = 10 * 1000
 
-    play(track)
+        track = AudioSegment.from_file("Songs/Track-1.mp3", "mp3")
+
+        track_duration = len(track)
+
+        countdown_starts = [timestamp for timestamp in range(0, track_duration, ten_seconds)] 
+
+        print(countdown_starts)
+
+        # play(track)
 
 
 if __name__ == '__main__':
-    main()
+    converter = Converter()
+
+    converter.generate()
